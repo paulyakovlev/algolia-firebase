bd7804ecf310edfb2f2d27477dec272bd77d4f54
Paul Yakovlev <yvpavel@gmail.com>
2020-04-12 18:46:56 -0700

    add reports from Firebase to Algolia

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ const algolia = algoliasearch(
   process.env.ALGOLIA_API_KEY
 );
 const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+
+// Get all reports from Firebase
+database.ref("/reports").once("value", (contacts) => {
+  // Build an array of all records to push to Algolia
+  const records = [];
+  contacts.forEach((contact) => {
+    // get the key and data from the snapshot
+    const childKey = contact.key;
+    const childData = contact.val();
+    // We set the Algolia objectID as the Firebase .key
+    childData.objectID = childKey;
+    // Add object for indexing
+    records.push(childData);
+  });
+
+  // Add or update new objects
+  index
+    .saveObjects(records)
+    .then(() => {
+      console.log("Reports imported into Algolia");
+    })
+    .catch((error) => {
+      console.error("Error when importing contact into Algolia", error);
+      process.exit(1);
+    });
+});
