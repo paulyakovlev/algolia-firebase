e2ff0db34e1e0291cb3c65eb685373050be23c5b
Paul Yakovlev <yvpavel@gmail.com>
2020-04-12 18:45:47 -0700

    set credentials

---
+++ index.js
@@ -0,0 +1,19 @@
+const algoliasearch = require("algoliasearch");
+const dotenv = require("dotenv");
+const firebase = require("firebase");
+
+// load values from the .env file in this directory into process.env
+dotenv.config();
+
+// configure firebase
+firebase.initializeApp({
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+const database = firebase.database();
+
+// configure algolia
+const algolia = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
+const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
